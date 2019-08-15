a5e51cbc532f7a90c288cf5af6815b32990e9042
Sam Debruyn <sam@debruyn.dev>
2019-08-15 11:59:29 +0200

    added support for setting minimum log level

--- Serilog.Sink.AppCenter/AppCenterSinkConfigurationExtensions.cs
+++ Serilog.Sink.AppCenter/AppCenterSinkConfigurationExtensions.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace Serilog.Sink.AppCenter
 {
     public static class AppCenterSinkConfigurationExtensions
     {
-        public static LoggerConfiguration AppCenterSink(
-              this LoggerSinkConfiguration loggerConfiguration,
-              AppCenterTarget target = default,
-              string appCenterSecret = null,
-              IFormatProvider formatProvider = null)
+        public static LoggerConfiguration AppCenterSink(this LoggerSinkConfiguration loggerConfiguration,
+            LoggingLevelSwitch levelSwitch = null, LogEventLevel logEventLevel = LogEventLevel.Verbose,
+            AppCenterTarget target = default,
+            string appCenterSecret = null,
+            IFormatProvider formatProvider = null)
         {
-            return loggerConfiguration.Sink(new AppCenterSink(target, appCenterSecret, formatProvider));
+            if (loggerConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(loggerConfiguration));
+            }
+            
+            return loggerConfiguration.Sink(new AppCenterSink(target, appCenterSecret, formatProvider), logEventLevel, levelSwitch);
         }
     }
 }
